de95da45d1e1fe0ec39dbd707662d7151d9e8d90
Paulo <pauloecomercial367@gmail.com>
2024-07-13 16:20:08 -0300

    :sparkles: Basic Identity Framework configuration

--- PropertiesService/Adapters/DataEF/AppDbContext.cs
+++ PropertiesService/Adapters/DataEF/AppDbContext.cs
@@ -1,17 +1,21 @@
 ﻿using DataEF.Configurations;
 using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataEF;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public DbSet<User> Users { get; set; }
 
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new UserConfiguration());
         modelBuilder.ApplyConfiguration(new PropertyConfiguration());
         modelBuilder.ApplyConfiguration(new ImageConfiguration());
