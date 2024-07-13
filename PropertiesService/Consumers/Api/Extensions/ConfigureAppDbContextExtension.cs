15cd825be48ca5b1c573e3cb4556940bba9109f5
Paulo <pauloecomercial367@gmail.com>
2024-07-12 22:17:56 -0300

    :sparkles: Mapping image, property and user db configuration, and creating migrations and configuring dbcontext injection

---
+++ PropertiesService/Consumers/Api/Extensions/ConfigureAppDbContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using DataEF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Extensions;
+
+public static class ConfigureAppDbContextExtension
+{
+    public static void ConfigureAppDbContext(this WebApplicationBuilder builder)
+    {
+        var connString = builder.Configuration.GetConnectionString("SqlServer");
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(connString);
+        });
+    }
+}
